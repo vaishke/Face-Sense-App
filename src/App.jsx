b30496ed77d1cacab5d9bdd601bac70b6974ede9
vaish_ke <vaishnavi.kesiraju@gmail.com>
2025-02-22 23:09:26 +0530

    Basic Working Webpage

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="Header">
+        <h1 className="logo">Face Sense</h1>
+        <nav className="nav-links">
+          <a href="App.jsx">Home</a>
+          <a href="About.jsx">About</a>
+          <a href="Detect.jsx">Detect</a>
+        </nav>
+      </div>
+      <main>
+        <p>Welcome to Face Sense!</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
